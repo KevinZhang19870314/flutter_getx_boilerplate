647e260ce909794c73b905ee217426f68d4a312d
kevin.zhang <kevinzhang19870314@gmail.com>
2021-04-12 17:51:52 +0800

    feat: add screenshot

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx_boilerplate/routes/routes.dart';
 import 'package:flutter_getx_boilerplate/shared/shared.dart';
-import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
